6aadb7edd26ecb78273fe19d7670f4d8a2d8b02f
Jan Gabriel H. Que <jan.que@whiterain-dti.com>
2024-11-07 11:05:16 +0800

    use single vite config

--- apps/secondapp/vite.config.ts
+++ apps/secondapp/vite.config.ts
@@ -1,8 +1,1 @@
-import { defineConfig } from "vite"
-import deno from "@deno/vite-plugin"
-import react from "@vitejs/plugin-react"
-
-// https://vite.dev/config/
-export default defineConfig({
-	plugins: [deno(), react()],
-})
+export { default } from "../../vite.config.base.ts"
